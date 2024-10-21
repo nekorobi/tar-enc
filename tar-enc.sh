a5fd45ac5fadad28c99c7992144acd6f8d8d96e9
Nekorobi <177852383+nekorobi@users.noreply.github.com>
2024-10-21 12:43:06 +0900

    refactor:

--- tar-enc.sh
+++ tar-enc.sh
@@ -48,18 +48,18 @@ MIT License © 2024 Nekorobi
 END
 }
 
-error() { echo -e "\e[1;31mError:\e[m $1" 1>&2; [[ $2 ]] && exit $2 || exit 1; }
+error() { local s=$1; shift 1; echo -e "Error: $@" 1>&2; exit $s; }
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
-  -D|--directory|-n|--name) [[ $# = 1 || $2 =~ ^- ]] && error "$1: requires an argument";;&
+  -D|--directory|-n|--name) [[ $# = 1 || $2 =~ ^- ]] && error 1 "$1: requires an argument";;&
   --bzip2)        compress=bzip2; shift 1;;
   -C|--camellia)  cipher=camellia256; shift 1;;
-  -d|--decrypt)   [[ $mode = list ]] && error "--decrypt, --list: exclusive options"; mode=decrypt; shift 1;;
+  -d|--decrypt)   [[ $mode = list ]] && error 1 "--decrypt, --list: exclusive options"; mode=decrypt; shift 1;;
   -D|--directory) dir=$(readlink -m "$2"); shift 2;; # dir: Not ending with a / (except root)
-  -l|--list)      [[ $mode = decrypt ]] && error "--decrypt, --list: exclusive options"; mode=list; shift 1;;
+  -l|--list)      [[ $mode = decrypt ]] && error 1 "--decrypt, --list: exclusive options"; mode=list; shift 1;;
   -n|--name)      name=$2; shift 2;;
-  -p|--pass-env)  [[ $PASS ]] || error "$1: requires 'PASS' environment variable"; export PASS; passOpt="-pass env:PASS"; shift 1;;
+  -p|--pass-env)  [[ $PASS ]] || error 1 "$1: requires 'PASS' environment variable"; export PASS; passOpt="-pass env:PASS"; shift 1;;
   --xz)           compress=xz; shift 1;;
   #
   -h|--help)      help; exit 0;;
@@ -69,55 +69,55 @@ while [[ $# -gt 0 ]]; do
   # ignore
   "") shift 1;;
   # invalid
-  -*) error "$1: unknown option";;
+  -*) error 1 "$1: unknown option";;
   # Operand
-  *)  [[ -f $1 || -d $1 ]] || error "$1: not a file or directory" 2; files[${#files[@]}]=$1; shift 1;;
+  *)  [[ -f $1 || -d $1 ]] || error 2 "$1: not a file or directory"; files[${#files[@]}]=$1; shift 1;;
   esac
 done
-[[ ${#files[@]} -gt 0 ]] || error "file (directory) not specified" 2
-for e in "${files[@]}"; do [[ -r $e ]] || error "$e: permission denied" 2; done
+[[ ${#files[@]} -gt 0 ]] || error 2 "file (directory) not specified"
+for e in "${files[@]}"; do [[ -r $e ]] || error 2 "$e: permission denied"; done
 
 # --directory
 if [[ ! $dry && $mode != list ]]; then
-  mkdir -p "$dir" && [[ -w $dir && -x $dir ]] || error "--directory: permission denied" 2
+  mkdir -p "$dir" && [[ -w $dir && -x $dir ]] || error 2 "--directory: permission denied"
 fi
 
 Openssl="openssl enc -$cipher -pbkdf2 $passOpt"
 
 encrypt() {
   # --name
-  [[ $name =~ / ]] && error "--name: cannot contain '/'"
+  [[ $name =~ / ]] && error 1 "--name: cannot contain '/'"
   [[ ! $name ]] && name=$(basename ${files[0]}).$(date +%F).$compress.${cipher:0:-3}
   [[ $dir = / ]] && out=/$name || out=$dir/$name
-  [[ -f $out ]] && error "--directory, --file: file exists: $out"
+  [[ -f $out ]] && error 1 "--directory, --file: file exists: $out"
   #
   quote() { for e in "$@"; do echo -e \'$e\' " "; done; }
   Tar="tar --create --$compress --to-stdout"
   [[ ! $quiet ]] && echo $Tar $(quote "${files[@]}") \| $Openssl -out \'$out\'
   [[ $dry ]] && exit
-  $Tar "${files[@]}" | $Openssl -out "$out" || error "encryption failed" 20
+  $Tar "${files[@]}" | $Openssl -out "$out" || error 20 "encryption failed"
   [[ ! $quiet ]] && ls -lh "$out" || true
 }
 
 decrypt() {
-  for e in "${files[@]}"; do [[ -f $e ]] || error "$e: not a file" 2; done
+  for e in "${files[@]}"; do [[ -f $e ]] || error 2 "$e: not a file"; done
   tarOpt=(--extract --$compress --directory)
   for e in "${files[@]}"; do
     if [[ $mode = list ]]; then
       [[ ! $quiet ]] && echo $Openssl -d -in \'$e\' \| tar --list --$compress
       [[ $dry ]] && break
-      $Openssl -d -in "$e" | tar --list --$compress || error "--list: failed: $e" 21
+      $Openssl -d -in "$e" | tar --list --$compress || error 21 "--list: failed: $e"
     else
       [[ ! $quiet ]] && echo $Openssl -d -in \'$e\' \| tar ${tarOpt[@]} \'$dir\'
       [[ $dry ]] && break
-      $Openssl -d -in "$e" | tar ${tarOpt[@]} "$dir" || error "--decrypt: failed: $e" 22
+      $Openssl -d -in "$e" | tar ${tarOpt[@]} "$dir" || error 22 "--decrypt: failed: $e"
     fi
   done
 }
 
 # check openssl v3, tar
 { type openssl && type tar && openssl version -v | grep "^OpenSSL 3\."; } >/dev/null 2>&1 ||
-  error "Required commands: openssl (version 3), tar" 3
+  error 3 "Required commands: openssl (version 3), tar"
 
 if [[ $mode = encrypt ]]; then encrypt; else decrypt; fi
 # error status: option 1, operand 2, version 3, encrypt 20, list 21, decrypt 22
